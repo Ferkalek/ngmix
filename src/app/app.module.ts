02f1e7ce57fe987bdd22505ea045cb6bc7fb3499
Ferkalek <ferkalekanton@gmail.com>
2020-05-11 20:46:03 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule } from '@angular/material/input';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { HttpService } from './http.service';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { RegistrationComponent } from './authentification/registration/registration.component';
-import { LoginComponent } from './authentification/login/login.component';
 import { HomeComponent } from './home/home.component';
-import { UsersListComponent } from './users/users-list/users-list.component';
-import { UserPageComponent } from './users/user-page/user-page.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent,
-    RegistrationComponent,
-    LoginComponent,
     HomeComponent,
-    UsersListComponent,
-    UserPageComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
-    FormsModule,
     BrowserAnimationsModule,
-    MatInputModule,
-    MatCardModule,
-    MatButtonModule,
   ],
-  providers: [
-    HttpService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
